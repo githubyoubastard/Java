338860c85b15a2e9e116d51c68cb0dbf016e24e8
chad <sooyeolyoo@gmail.com>
2018-06-11 15:47:10 +0900

    s

--- Step06_Operator_My/src/test/main/MainClass01.java
+++ Step06_Operator_My/src/test/main/MainClass01.java
@@ -17,7 +17,7 @@ public class MainClass01 {
 		int sum2= 10+ num2;
 		
 		// 정수 / 정수 => 정수이므로 5를 3으로 나눈 몫을 
-		int result1 = 5/3;
+		int result1 = 5 / 3;
 		// 정수 / 정수 => 정수이므로 5를 2으로 나눈 몫을 
 		int result2= num1/num2;
 		
@@ -26,5 +26,8 @@ public class MainClass01 {
 		//data가 어느 하나만이라도 double type이면 정확한 결과를 얻을 수 있다.
 		double result3= 5 / 3.0;
 		double result4= num1 / (double)num2;
+		
+		//num1 을 num2로 나눈 나머지값 얻어내기
+		int result5 = num1 % num2;
 	}
 }
