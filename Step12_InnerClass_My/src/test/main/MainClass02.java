3ce492f2dd98f63aa8cf073c2e64760b2d56008e
chad <sooyeolyoo@gmail.com>
2018-06-15 15:49:32 +0900

    asd

--- Step12_InnerClass_My/src/test/main/MainClass02.java
+++ Step12_InnerClass_My/src/test/main/MainClass02.java
@@ -6,16 +6,21 @@ import test.mypac.Bakery.Cake;
 
 public class MainClass02 {
 	public static void main(String[] args) {
+
+//		Bakery b=new Bakery();
+//		//Bakery 객체를 이용해서 bread 객체의 참조값 얻어오기
+//		Bread bread=b.getBread();
+//		//Bakery 객체를 이용해서 cake객체의 참조값 얻어오기
+//		Cake cake=b.getCake();
+//		
+//		//각각 메소드 호출하기
+//		bread.eat();
+//		
+//		cake.eat();
 		
-		Bakery b=new Bakery();
-		//Bakery 객체를 이용해서 bread 객체의 참조값 얻어오기
-		Bread bread=b.getBread();
-		//Bakery 객체를 이용해서 cake객체의 참조값 얻어오기
-		Cake cake=b.getCake();
-		
-		//각각 메소드 호출하기
+		Bread bread=new Bakery().getBread();
 		bread.eat();
-		
+		Cake cake=new Bakery().getCake();
 		cake.eat();
 	}
 }
