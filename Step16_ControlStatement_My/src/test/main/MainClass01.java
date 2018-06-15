ba622a9effd56285273776a31f6afb5f0c058575
chad <sooyeolyoo@gmail.com>
2018-06-15 16:46:00 +0900

    s

---
+++ Step16_ControlStatement_My/src/test/main/MainClass01.java
@@ -0,0 +1,25 @@
+package test.main;
+
+import java.util.Scanner;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		// 콘솔창으로 부터 입력 받을 수 있는 Scanner 객체 생성
+		Scanner scan=new Scanner(System.in);
+		System.out.println("점수 입력:");
+		//Scanner 객체를 이용해서 정수 입력 받기(점수)
+		int jumsu=scan.nextInt();
+		if(jumsu>=90) {
+			System.out.println("A입니다");
+		}else if(jumsu>=80) {
+			System.out.println("B입니다.");
+		}else if(jumsu>=70) {
+			System.out.println("C입니다.");
+		}else if(jumsu>=60) {
+			System.out.println("D입니다.");
+		}else if(jumsu>=50) {
+			System.out.println("F입니다.");
+		}
+		
+	}
+}
---
+++ Step16_ControlStatement_My/src/test/main/MainClass02.java
@@ -0,0 +1,12 @@
+package test.main;
+
+public class MainClass02 {
+	public static final int SWORD=0;
+	public static final int GUN=1;
+	public static final int ARROW=2;
+	public static final int FIST=3;
+	
+	public static void main(String[] args) {
+
+	}
+}
