c5beb03228ad041b55c74675cf0b880e5c9d0c27
chad <sooyeolyoo@gmail.com>
2018-06-15 17:52:28 +0900

    a

--- Step15_GenericClass_My/src/test/main/MainClass02.java
+++ Step15_GenericClass_My/src/test/main/MainClass02.java
@@ -2,10 +2,8 @@ package test.main;
 
 import test.mypac.Apple;
 import test.mypac.Banana;
-import test.mypac.BananaBox;
 import test.mypac.FruitBox;
 import test.mypac.Orange;
-import test.mypac.OrangeBox;
 
 public class MainClass02 {
 	public static void main(String argus) {
