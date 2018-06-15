cb478161d844238c3f55cc66fc2ca9b5f3fa93e9
chad <sooyeolyoo@gmail.com>
2018-06-15 17:00:16 +0900

    a

--- Step16_ControlStatement_My/src/test/main/MainClass04.java
+++ Step16_ControlStatement_My/src/test/main/MainClass04.java
@@ -2,9 +2,15 @@ package test.main;
 
 public class MainClass04 {
 	public static void main(String[] args) {
+		// int 배열
 		int[] nums= {10, 20, 30, 40, 50};
 		for(int i=0;i<nums.length;i++) {
 			int tmp=nums[i];
+			System.out.println("tmp:"+tmp);
+		}
+		System.out.println("== 확장 for 문 ==");
+		
+		for (int tmp:nums) {
 			System.out.println("tmp:"+tmp);
 		}
 	}
