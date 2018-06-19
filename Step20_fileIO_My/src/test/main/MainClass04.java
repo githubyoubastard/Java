30fd8c2857d6ab35c4c26e5d23ec76694ed3fa77
chad <sooyeolyoo@gmail.com>
2018-06-19 17:19:07 +0900

    6월 19일 Utile_Class ~ fileIO 까지

--- Step20_fileIO_My/src/test/main/MainClass04.java
+++ Step20_fileIO_My/src/test/main/MainClass04.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class MainClass04 {
 	public static void main(String[] args) {
 		try {
-			FileWriter fw=new FileWriter("c:/myFolder/monkey.txt");
+			FileWriter fw=new FileWriter("c:/myFolder/monkey.txt", true); //true를 넣어주면 append 기능이 추가됨 FileWriter. 해보면 여러가지 기능이 있다.
 			fw.write("원숭아 \r\n");
 			fw.write("당구 한 게임 콜? \r\n");
 			fw.flush();
