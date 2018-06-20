1bf4749c5f9095c36d09a6601db4d727c40a3a4b
chad <sooyeolyoo@gmail.com>
2018-06-20 17:22:40 +0900

    sad

--- Step22_Socket_My/src/example1/ServerMain.java
+++ Step22_Socket_My/src/example1/ServerMain.java
@@ -1,5 +1,8 @@
 package example1;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /*
  *  [ 소켓 서버 프로그램 작성 순서 ]
  *   1. ServerSocket 객체를 생성한다. (port 번호 필요)
@@ -10,5 +13,29 @@ package example1;
  *   4. 클라이언트와 접속을 종료하려면 Socket 객체를 닫아준다.
  */
 public class ServerMain {
-
+	public static void main(String[] args) {
+		//필요한 객체를 저장할 변수 만들기
+		ServerSocket serverSocket=null;
+		Socket socket=null;
+		try{
+			//5000 번 port 에서 클라이언트의 접속을 기다린다.
+			serverSocket=new ServerSocket(5000);
+			System.out.println("클라이언트의 Socket 연결 요청을 대기중...");
+			while(true){
+				//연결요청을 해오면 클라이언트와 연결된 Socket 객체의 참조값이 리턴된다.
+				socket=serverSocket.accept();
+				//접속된 클라이언트의 ip 주소 얻어오기
+				String clientIp=socket.getInetAddress().getHostAddress();
+				System.out.println("clientIp:"+clientIp);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(serverSocket!=null)serverSocket.close();
+				if(socket!=null)socket.close();
+			}catch(Exception e){}
+		}
+		System.out.println("Server main() 메소드가 종료 됩니다. ");
+	}	
 }
