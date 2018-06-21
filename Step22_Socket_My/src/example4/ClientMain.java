8a608b41459431d33a59bee894a47de5ee89117c
chad <sooyeolyoo@gmail.com>
2018-06-21 14:32:54 +0900

    연습 할 수 있는 example4 추가

---
+++ Step22_Socket_My/src/example4/ClientMain.java
@@ -0,0 +1,14 @@
+package example4;
+
+public class ClientMain {
+	public static void main(String[] args) {
+	
+		
+	}
+}
+
+
+
+
+
+
---
+++ Step22_Socket_My/src/example4/ServerMain.java
@@ -0,0 +1,10 @@
+package example4;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMain {
+	public static void main(String[] args) {
+
+	}
+}
