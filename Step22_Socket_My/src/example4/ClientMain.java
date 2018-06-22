62d79d2efec42ead02c1e798266b3b3eb245625d
chad <sooyeolyoo@gmail.com>
2018-06-22 09:44:29 +0900

    a

--- Step22_Socket_My/src/example4/ClientMain.java
+++ Step22_Socket_My/src/example4/ClientMain.java
@@ -1,7 +1,28 @@
 package example4;
 
+import java.io.BufferedWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ClientMain {
-	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		BufferedWriter br=null;
+		Socket socket=null;
+		try {
+			socket=new Socket("",5000);
+			System.out.println("접속 성공");
+			while(true) {
+				String msg=scan.nextLine();
+				System.out.println("입력할 문자(종료:q):");
+								
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+		}
+	}
 	
 }
 
--- Step22_Socket_My/src/example4/ServerThread.java
+++ Step22_Socket_My/src/example4/ServerThread.java
@@ -1,24 +1,6 @@
 package example4;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.Socket;
-
 public class ServerThread extends Thread{
-	private Socket socket;
 	
-	public ServerThread(Socket socket) {
-		this.socket=socket;
-	}
-	@Override
-	public void run() {
-		BufferedReader br=null;
-		try {
-				br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			
-		}
-	}
+	
 }
