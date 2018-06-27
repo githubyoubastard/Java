b72f3ba236fb42c2b43ef3016ede3ff69212b7dd
chad <sooyeolyoo@gmail.com>
2018-06-27 18:09:03 +0900

    f

--- Step26_MyPracticeForOracleConnection/src/test/dao/EmpDao.java
+++ Step26_MyPracticeForOracleConnection/src/test/dao/EmpDao.java
@@ -1,53 +1,12 @@
 package test.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import test.dto.EmpDto;
-import test.util.DBConnect;
-
 public class EmpDao {
-	private static EmpDao dao;
+	private EmpDao dao;
 	private EmpDao() {};
-	public static EmpDao getInstance() {
+	public EmpDao getInstance() {
 		if(dao==null) {
 			dao=new EmpDao();
 		}
 		return dao;
-	}
-	public List<EmpDto> getList(){
-		Connection conn = null;
-		PreparedStatement pstmt =null;
-		ResultSet rs = null;
-		List<EmpDto> list=new ArrayList<>();
-			try {
-				conn=new DBConnect().getConn();
-				String sql="SELECT ENAME,DNAME FROM EMP, DEPT WHERE EMP.DEPTNO=DEPT.DEPTNO AND SAL BETWEEN 3000 AND 5000";
-				pstmt=conn.prepareStatement(sql);
-				rs=pstmt.executeQuery();
-				while(rs.next()) {
-					String ename=rs.getString("ENAME");
-					String dname=rs.getString("DNAME");
-					EmpDto dto=new EmpDto();
-					dto.setEname(ename);
-					dto.setDname(dname);
-					list.add(dto);
-				}
-			}catch(SQLException se) {
-				se.printStackTrace();
-			}finally {
-				try {
-					if(conn!=null)conn.close();
-					if(pstmt!=null)pstmt.close();
-					if(rs!=null)pstmt.close();
-				}catch(SQLException se) {
-					se.printStackTrace();
-				}
-			}
-		return list;
 	}
 }
--- Step26_MyPracticeForOracleConnection/src/test/util/DBConnect.java
+++ Step26_MyPracticeForOracleConnection/src/test/util/DBConnect.java
@@ -6,19 +6,20 @@ import java.sql.SQLException;
 
 public class DBConnect {
 	private Connection conn;
+	
 	public DBConnect() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String url="jdbc:oracle:thin:@localhost:1521:xe";
-			conn=DriverManager.getConnection(url,"scott","tiger");
-			System.out.println("연결 확인");
+			conn=DriverManager.getConnection(url, "scott", "tiger");
+			System.out.println("connected successfully");
 		}catch(ClassNotFoundException cnfe) {
 			cnfe.printStackTrace();
 		}catch(SQLException se) {
 			se.printStackTrace();
 		}
 	}
-	public Connection getConn(){
+	public Connection getconn() {
 		return conn;
 	}
 }
