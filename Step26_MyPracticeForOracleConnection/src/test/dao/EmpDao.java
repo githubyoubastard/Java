aa8b6a422e03f7629cf1c2f3fde56de04639f9d2
chad <sooyeolyoo@gmail.com>
2018-07-18 11:48:28 +0900

    ??

--- Step26_MyPracticeForOracleConnection/src/test/dao/EmpDao.java
+++ Step26_MyPracticeForOracleConnection/src/test/dao/EmpDao.java
@@ -1,5 +1,15 @@
 package test.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.dto.EmpDto;
+import test.util.DBConnect;
+
 public class EmpDao {
 	private EmpDao dao;
 	private EmpDao() {};
@@ -8,5 +18,23 @@ public class EmpDao {
 			dao=new EmpDao();
 		}
 		return dao;
+	}
+	public List<EmpDto> getList(){
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<EmpDto> list=new ArrayList<>();
+		try {
+			conn=new DBConnect().getconn();
+			String sql="";
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
